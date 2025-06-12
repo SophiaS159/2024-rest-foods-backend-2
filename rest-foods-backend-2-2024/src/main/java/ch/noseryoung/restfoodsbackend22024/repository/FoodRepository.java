218ff4d08f850f60e0216c03e2f719b55a7ed023
Jonathan Seibl <jonathan.seibl@edu.tbz.ch>
2025-06-12 09:24:54 +0200

    Added Service for Food, Reservationbd and User
    Also repository layer added for everything

--- rest-foods-backend-2-2024/src/main/java/ch/noseryoung/restfoodsbackend22024/repository/FoodRepository.java
+++ rest-foods-backend-2-2024/src/main/java/ch/noseryoung/restfoodsbackend22024/repository/FoodRepository.java
@@ -1,4 +1,12 @@
 package ch.noseryoung.restfoodsbackend22024.repository;
 
-public class FoodRepository {
+import ch.noseryoung.restfoodsbackend22024.model.Food;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface FoodRepository extends JpaRepository<Food, Long> {
+    List<Food> findByNameContainingIgnoreCase(String name);
 }
--- rest-foods-backend-2-2024/src/main/java/ch/noseryoung/restfoodsbackend22024/service/FoodService.java
+++ rest-foods-backend-2-2024/src/main/java/ch/noseryoung/restfoodsbackend22024/service/FoodService.java
@@ -1,4 +1,40 @@
 package ch.noseryoung.restfoodsbackend22024.service;
 
+import ch.noseryoung.restfoodsbackend22024.model.Food;
+import ch.noseryoung.restfoodsbackend22024.repository.FoodRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
 public class FoodService {
+
+    private final FoodRepository foodRepository;
+
+    @Autowired
+    public FoodService(FoodRepository foodRepository) {
+        this.foodRepository = foodRepository;
+    }
+
+    public List<Food> getAllFoods() {
+        return foodRepository.findAll();
+    }
+
+    public Optional<Food> getFoodById(Long id) {
+        return foodRepository.findById(id);
+    }
+
+    public Food createFood(Food food) {
+        return foodRepository.save(food);
+    }
+
+    public void deleteFood(Long id) {
+        foodRepository.deleteById(id);
+    }
+
+    public List<Food> searchFoodsByName(String name) {
+        return foodRepository.findByNameContainingIgnoreCase(name);
+    }
 }
